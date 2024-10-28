4bd3fea2ee48d9a2a95407a596725f7f013a5bd5
Dosyaan <dastanmatraimov79@gmail.com>
2024-10-28 19:33:07 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+// task - 1
+
+// num = (prompt("Введите число"));
+// console.log(Math.floor(num))
+
+// task - 2
+
+// const num = [3.1, 2.7, 4.4, 5.5]
+// const arr = []
+
+// num.forEach((item) =>{
+//     arr.push(Math.floor(item))
+// })
+
+// console.log(arr)
+
+// task - 3
+
+// num1 = +prompt("Введите первое число");
+// num2 = +prompt("Введите второе число");
+
+// let minus = num1 - num2
+// console.log(Math.floor(minus))
+
+// task - 4
+
+// const num = [1.5, 2.5, 3.5, 4.5]
+// const mid = num.reduce((prev, item) =>{
+//     return prev + item
+// })
+
+// console.log(Math.floor(mid))
+
+// task - 5
+
+// const price = [12.3, 45.7, 22.8, 19.9]
+// const int = price.map((item) =>{
+//     return Math.floor(item)
+// })
+// console.log(int)
+
+// task - 6
+
+// let num = +prompt();
+// let int = Math.floor(num / 5) * 5
+
+// console.log(int)
+
+// task - 7
+
+// let num = +prompt()
+// let int = Math.floor(num * 10) / 10
+
+// console.log(int)
+
+// taks - 8
